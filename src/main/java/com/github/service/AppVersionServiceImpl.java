4b5b565efbe33193614b74ca9c50ef8260f73af2
小城里的旧知己 <1196638242@qq.com>
2020-08-19 09:36:16 +0800

    第一次提交只是做了项目的基本功能 后续还有很多攻难待完善

---
+++ src/main/java/com/github/service/AppVersionServiceImpl.java
@@ -0,0 +1,24 @@
+package com.github.service;
+
+import com.github.dao.AppVersionMapper;
+import com.github.pojo.AppVersion;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public abstract class AppVersionServiceImpl implements AppVersionService {
+
+    @Resource
+    private AppVersionMapper appVersionMapper;
+    @Override
+    public List<AppVersion> appversionadd(Integer id) {
+        return appVersionMapper.appversionadd(id);
+    }
+
+    @Override
+    public AppVersion findAppVersionById(Integer vid) {
+        return appVersionMapper.findAppVersionById(vid);
+    }
+}
